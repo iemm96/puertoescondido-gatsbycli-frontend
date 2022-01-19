ff761ff9e978bdf6ec9728cc828527804f2b8c1a
Emmanuel <iemm96@gmail.com>
2022-01-19 17:14:19 -0600

    Added useWindowDiemensions hook, sliderComponent and LastPosts files

---
+++ src/components/SliderComponent.tsx
@@ -0,0 +1,69 @@
+import * as React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import Grid from "@mui/material/Grid";
+import ChevronRight from "@mui/icons-material/ChevronRight";
+import ChevronLeft from "@mui/icons-material/ChevronLeft";
+import PropertyCard from "./components/PropertyCard"
+
+const settings = {
+  dots: false,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  arrows: false,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        centerMode: true,
+        centerPadding: '15%',
+        slidesToShow: 1,
+        infinite: true,
+      }
+    }
+  ]
+};
+
+const PropertySlider = () => {
+  const sliderRef = React.useRef();
+
+  return(
+    <>
+      <Typography variant="h6">¡Tu mejor opción!</Typography>
+      <Grid container justifyContent="space-between">
+        <Grid item>
+          <Typography sx={{fontWeight: 600, mb: 5}} variant="h5">Propiedades destacadas</Typography>
+        </Grid>
+        <Grid item>
+          <IconButton onClick={() => {
+              // @ts-ignore
+              sliderRef.current.slickPrev();
+          }
+          } component={ChevronLeft}/>
+          <IconButton onClick={() => {
+            // @ts-ignore
+            sliderRef?.current?.slickNext()
+          }} component={ChevronRight}/>
+        </Grid>
+      </Grid>
+      <Slider ref={sliderRef} {...settings}>
+        <PropertyCard/>
+        <PropertyCard/>
+        <PropertyCard/>
+        <PropertyCard/>
+        <PropertyCard/>
+        <PropertyCard/>
+        <PropertyCard/>
+        <PropertyCard/>
+        <PropertyCard/>
+        <PropertyCard/>
+        <PropertyCard/>
+      </Slider>
+    </>
+  )
+}
+
+export default React.forwardRef(PropertySlider);
