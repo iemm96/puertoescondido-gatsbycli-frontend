0fc71009d41033be5b75b6679655a0bc7318bedc
iemm96 <iemm96@gmail.com>
2022-06-16 16:15:27 -0500

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -11,25 +11,23 @@ exports.createPages = async ({ graphql, actions }) => {
       query Properties {
               allProperty {
                   nodes {
-                     
-                          coverImage {
-                              childImageSharp {
-                                  gatsbyImageData(width: 280, placeholder: BLURRED, formats: [AUTO, WEBP, AVIF])
-                              }
+                      coverImage {
+                          childImageSharp {
+                              gatsbyImageData(width: 280, placeholder: BLURRED, formats: [AUTO, WEBP, AVIF])
                           }
+                      }
+                      name
+                      price
+                      uid
+                      measures_unit
+                      isFeatured
+                      slug
+                      location {
                           name
-                          price
-                          uid
-                          measures_unit
-                          isFeatured
-                          slug
-                          location {
-                              name
-                          }
-                          features {
-                              name
-                          }
-                      
+                      }
+                      features {
+                          name
+                      }
                   }
               }
           }
@@ -48,10 +46,10 @@ exports.createPages = async ({ graphql, actions }) => {
                 name
               }
               coverImage {
-                              childImageSharp {
-                                  gatsbyImageData(width: 280, placeholder: BLURRED, formats: [AUTO, WEBP, AVIF])
-                              }
-                          }
+                  childImageSharp {
+                      gatsbyImageData(width: 280, placeholder: BLURRED, formats: [AUTO, WEBP, AVIF])
+                  }
+              }
               images {
                 childrenImageSharp {
                   gatsbyImageData(formats: AUTO)
@@ -66,16 +64,15 @@ exports.createPages = async ({ graphql, actions }) => {
 
     data.allProperty.nodes.forEach( node => {
       createPage({
-        path: `/propiedades/${ node.slug }`,
+        path: `/propiedad/${ node.slug }`,
         component: require.resolve("./src/templates/PropertyDetails.tsx"),
         context: { slug: node.slug },
       })
     } );
 
-
     dataProjects.data.allProject.nodes.forEach( node => {
       createPage({
-        path: `/proyectos-ssr/${ node.slug }`,
+        path: `/proyecto/${ node.slug }`,
         component: require.resolve("./src/templates/ProjectDetails.tsx"),
         context: { slug: node.slug },
       })
@@ -83,12 +80,10 @@ exports.createPages = async ({ graphql, actions }) => {
   }catch (e) {
     console.log( e );
   }
-
-
-
+  
 }
 
-exports.sourceNodes = async ({ actions, createNodeID, createContentDigest }) => {
+exports.sourceNodes = async ({ actions, createContentDigest }) => {
   const { createNode } = actions;
 
   try {
@@ -204,46 +199,6 @@ exports.createSchemaCustomization = ({ actions, schema }) => {
     }),
   ];
 
-  /*
-  createTypes(`
-    type Location implements Node {
-      name: String!
-    }
-    type Feature implements Node {
-      name: String!
-    }
-
-    type Property implements Node {
-      uid: String!
-      name: String!
-      description: String!
-      price: String!
-      measures_unit: String!
-      isFeatured: Boolean!
-      location: Location!
-      features: Feature!
-      slug: String!
-      area: String!
-      coverImage: File @link(from: "fields.localFile")
-    }
-    type Project implements Node {
-      uid: String!
-      name: String!
-      description: String!
-      price: String!
-      isFeatured: Boolean!
-      isProject: Boolean!
-      location: Location!
-      features: [Feature!]!
-      slug: String!
-      length: String!
-      width: String!
-      measures_unit: String!
-      area: String!
-      coverImage: File @link(from: "fields.localFile")
-    }
-  `)*/
-
   createTypes( typeDefs )
 }
 
@@ -253,36 +208,26 @@ exports.onCreateNode = async ({
                                 createNodeId,
                                 getCache,
                               }) => {
-  // For all MarkdownRemark nodes that have a featured image url, call createRemoteFileNode
-
-
-
-   try{
-
-     if (
-       node.internal.type === "Property" &&
-       node.coverImage.url !== null
-     ) {
-
-       const fileNode = await createRemoteFileNode({
-         url: node.coverImage.url, // string that points to the URL of the image
-         parentNodeId: node.id, // id of the parent node of the fileNode you are going to create
-         createNode, // helper function in gatsby-node to generate the node
-         createNodeId, // helper function in gatsby-node to generate the node id
-         getCache,
-       })
-
-       // if the file was created, extend the node with "localFile"
-       if (fileNode) {
-         createNodeField({ node, name: "localFile", value: fileNode.id })
-       }
-     }
-   }catch (e) {
-     console.log('error creating node ',e);
-   }
 
   try{
+    if (
+        node.internal.type === "Property" &&
+        node.coverImage.url !== null
+    ) {
 
+      const fileNode = await createRemoteFileNode({
+        url: node.coverImage.url, // string that points to the URL of the image
+        parentNodeId: node.id, // id of the parent node of the fileNode you are going to create
+        createNode, // helper function in gatsby-node to generate the node
+        createNodeId, // helper function in gatsby-node to generate the node id
+        getCache,
+      })
+
+      // if the file was created, extend the node with "localFile"
+      if (fileNode) {
+        createNodeField({ node, name: "localFile", value: fileNode.id })
+      }
+    }
     if (
         node.internal.type === "Project" &&
         node.coverImage.url !== null
