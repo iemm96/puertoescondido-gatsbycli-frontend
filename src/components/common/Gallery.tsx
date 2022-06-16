45541a705e63e525de5e98425b3212308b5b5028
iemm96 <iemm96@gmail.com>
2022-06-16 13:08:32 -0500

    Added Gatsby Image support

--- src/components/common/Gallery.tsx
+++ src/components/common/Gallery.tsx
@@ -1,17 +1,26 @@
 import Container from "@mui/material/Container";
 import {Scrollbar} from "swiper";
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 import 'swiper/css';
 import 'swiper/css/scrollbar';
+import "swiper/css/free-mode";
+import "swiper/css/navigation";
+import "swiper/css/thumbs";
+import { FreeMode, Navigation, Thumbs } from "swiper";
+
 import Box from "@mui/material/Box";
 import * as React from "react";
 import {Modal} from "@mui/material";
 import {useState} from "react";
+import useTheme from "@mui/material/styles/useTheme"
+import {GatsbyImage, getImage} from "gatsby-plugin-image";
 const boxStyles = {
     p: {
         xs: '0 1rem 0 0'
     },
     width: '100%',
+    display: 'flex',
+    justifyContent: 'center'
 }
 
 const modalStyles = {
@@ -27,9 +36,12 @@ export const useGallery = () => {
 
 }
 
-export const Gallery = ({data}:{data:any}) => {
+export const Gallery = ({ data, preview }:{ data:any, preview:boolean }) => {
+    console.log( data )
+    const theme = useTheme();
     const [ open, setOpen ] = useState<boolean>( false );
     const [ swiperDef, setSwiperDef ] = React.useState<any>( [] );
+    const [ thumbsSwiper, setThumbsSwiper ] = useState<any>(null);
     const [ selectedImage, setSelectedImage ] = useState<string | null>( null );
     const [ swiperState, setSwiperState ] = React.useState<any>( {
         isBeginning: true,
@@ -58,63 +70,100 @@ export const Gallery = ({data}:{data:any}) => {
                     />
                 </Box>
             </Modal>
-            <Swiper
-                modules={[ Scrollbar ]}
-                slidesPerView="auto"
-                spaceBetween={ 10 }
-                centeredSlides={ true }
-                onSlideChange={ () => {
-                    setSwiperState( {
-                        isEnd: swiperDef.isEnd,
-                        isBeginning: swiperDef.isBeginning
-                    } );
-                } }
-                onSwiper={ (swiper) => {
-                    setSwiperDef( swiper );
-                    setSwiperState( {
-                        isEnd: false,
-                        isBeginning: true
-                    });
-                }}
-                breakpoints={{
-                    "640": {
-                        "slidesPerView": 2,
-                        "spaceBetween": 10
-                    },
-                    "768": {
-                        "slidesPerView": 2,
-                        "spaceBetween": 40
-                    },
-                    "1024": {
-                        "slidesPerView": 4,
-                        "spaceBetween": 10
+            <>
+                <Swiper
+                    style={{
+                        // @ts-ignore
+                        "--swiper-navigation-color": theme.palette.primary.main,
+                        "--swiper-pagination-color": theme.palette.primary.main,
+                        width: '100%',
+                        height: 450
+                    }}
+                    spaceBetween={10}
+                    navigation={true}
+                    thumbs={{ swiper: thumbsSwiper }}
+                    modules={[FreeMode, Navigation, Thumbs]}
+                >
+                    {
+                        data && data.map((val:any, index:number) => (
+                            <SwiperSlide key={ index }>
+                                <Box key={ index } sx={boxStyles}>
+
+                                    <GatsbyImage
+                                        //onClick={ () => handleSelectImage( val.url ) }
+                                        alt={"img-1"}
+                                        style={{
+                                            height: 400,
+                                            cursor: 'pointer',
+                                            borderRadius: 16,
+                                            objectFit: 'cover'
+                                        }}
+                                        image={getImage( val )}
+                                    />
+                                </Box>
+                            </SwiperSlide>
+                        ))
                     }
-                }}
-                freeMode={true}
-            >
-                {
-                    data && data.map((val:any, index:number) => (
-                        <SwiperSlide key={ index }>
-                            <Box key={ index } sx={boxStyles}>
+                </Swiper>
+                <Swiper
+                    modules={[ Scrollbar ]}
+                    slidesPerView="auto"
+                    spaceBetween={ 10 }
+                    centeredSlides={ true }
+                    onSlideChange={ () => {
+                        setSwiperState( {
+                            isEnd: swiperDef.isEnd,
+                            isBeginning: swiperDef.isBeginning
+                        } );
+                    } }
+                    onSwiper={ (swiper) => {
+                        setThumbsSwiper(swiper)
+                        setSwiperDef( swiper );
+                        setSwiperState( {
+                            isEnd: false,
+                            isBeginning: true
+                        });
+                    }}
+                    breakpoints={{
+                        "640": {
+                            "slidesPerView": 2,
+                            "spaceBetween": 10
+                        },
+                        "768": {
+                            "slidesPerView": 2,
+                            "spaceBetween": 40
+                        },
+                        "1024": {
+                            "slidesPerView": 4,
+                            "spaceBetween": 6
+                        }
+                    }}
+                    freeMode={true}
+                >
+                    {
+                        data && data.map((val:any, index:number) => (
+                            <SwiperSlide key={ index }>
+                                <Box key={ index } sx={boxStyles}>
 
-                                <img
-                                    onClick={ () => handleSelectImage( val.url ) }
-                                    alt={"img-1"}
-                                    width={200}
-                                    height={200}
-                                    style={{
-                                        cursor: 'pointer',
-                                        borderRadius: 16,
-                                        objectPosition: 'cover'
-                                    }}
-                                    src={val.url}
-                                />
-                            </Box>
 
-                        </SwiperSlide>
-                    ))
-                }
-            </Swiper>
+                                    <GatsbyImage
+                                        //onClick={ () => handleSelectImage( val.url ) }
+                                        alt={"img-1"}
+                                        style={{
+                                            cursor: 'pointer',
+                                            borderRadius: 8,
+                                            objectFit: 'cover'
+                                        }}
+                                        image={getImage( val )}
+                                    />
+                                </Box>
+
+                            </SwiperSlide>
+                        ))
+                    }
+                </Swiper>
+            </>
+
         </>
 
     )
