018f8d7bc186de29d4db9e6f6998a2a1fdc8b009
iemm96 <iemm96@gmail.com>
2022-06-18 02:01:33 -0500

    Minor css tweaks

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,12 +5,12 @@
  * See: https://www.gatsbyjs.com/docs/use-static-query/
  */
 
-import * as React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import * as React from "react";
+import PropTypes from "prop-types";
+import { useStaticQuery, graphql } from "gatsby";
 
-import Header from "./Header"
-import "./layout.css"
+import Header from "./Header";
+import "./layout.css";
 import Footer from "./Footer"
 import { ThemeProvider } from "@mui/material/styles"
 import { defaultTheme, defaultThemeDark } from "../theme/Theme"
