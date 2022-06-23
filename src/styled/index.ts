c4a23d41a117b5831702be99dfc96e2e734a2e00
iemm96 <iemm96@gmail.com>
2022-06-22 23:38:22 -0500

    Update index.ts

--- src/styled/index.ts
+++ src/styled/index.ts
@@ -3,11 +3,9 @@ import Card from "@mui/material/Card";
 
 export const StyledCard = styled(Card)(() => ({
   position: 'relative',
-  borderRadius: 16,
   width: 'auto',
   maxWidth: 414,
   minWidth: 280,
-  height: 'auto',
 }));
 
 export const StyledButton = styled(Button)(() => ({
