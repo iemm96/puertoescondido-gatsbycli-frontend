22e154a38486e2469be44d9d62fbbf6dadf31a48
iemm96 <iemm96@gmail.com>
2022-06-29 13:39:00 -0500

    Update SliderComponent.tsx

--- src/components/SliderComponent.tsx
+++ src/components/SliderComponent.tsx
@@ -20,160 +20,164 @@ import Button from "@mui/material/Button";
 import useTheme from "@mui/material/styles/useTheme";
 
 type SliderComponentType = {
-  title: string;
-  subtitle: string;
-  settings?: any;
-  data?: any;
+    title: string;
+    subtitle: string;
+    settings?: any;
+    data?: any;
 }
 
 const boxStyles = {
-  p: {
-    xs: '0 1rem 0 0'
-  },
-  width: '100%',
+    p: {
+        xs: '0 1rem 0 0'
+    },
+    width: '100%',
 }
 
 
 const PropertySlider = ({ title, subtitle, data  }:SliderComponentType) => {
-  const theme = useTheme();
-  const [ swiperDef, setSwiperDef ] = React.useState<any>( [] );
-  const [ swiperState, setSwiperState ] = React.useState<any>( {
-    isBeginning: true,
-    isEnd: false
-  } );
+    const theme = useTheme();
+    const [ swiperDef, setSwiperDef ] = React.useState<any>( [] );
+    const [ swiperState, setSwiperState ] = React.useState<any>( {
+        isBeginning: true,
+        isEnd: false
+    } );
 
-  const arrowButtonStyles = {
-    borderRadius: 2,
-    height: 40,
-  }
+    const arrowButtonStyles = {
+        borderRadius: 2,
+        height: 40,
+    }
 
-  return(
-      <>
-        <Container maxWidth="xl" sx={ {pl: 2} }>
-          <Typography variant="subtitle1">{title}</Typography>
-          <Grid
-              sx={{
-                mb:{
-                  xs: 2
-                }
-              }}
-              container
-              justifyContent="space-between"
-          >
-            <Grid item>
-              <Typography sx={{fontWeight: 600, mb: 1}} variant="h5">{subtitle}</Typography>
-            </Grid>
-            <Grid sx={{
-              display: {
-                xs: 'none',
-                md: 'flex'
-              },
-            }} item
-            >
-              <Button
-                  sx={{ textTransform: 'none' }}
-                  color="primary"
-                  onClick={ () => navigate( '/propiedades' ) }
-                  variant="contained"
-              >
-                Ver más propiedades
-              </Button>
-              <Box>
-                <IconButton
-                    sx={ arrowButtonStyles }
-                    disabled={ swiperState.isBeginning }
-                    onClick={ () => swiperDef ? swiperDef.slidePrev() : '' }
-                >
-                  <ChevronLeft/>
-                </IconButton>
-                <IconButton
+    return(
+        <>
+            <Container maxWidth="xl" sx={ {pl: 2} }>
+                <Typography variant="subtitle1">{title}</Typography>
+                <Grid
                     sx={{
-                      borderRadius: 2,
-                      height: 40
+                        mb:{
+                            xs: 2
+                        }
                     }}
-                    disabled={ swiperState.isEnd }
-                    onClick={ () => swiperDef ? swiperDef.slideNext() : '' }
+                    container
+                    justifyContent="space-between"
                 >
-                  <ChevronRight/>
-                </IconButton>
-              </Box>
-            </Grid>
-          </Grid>
-          <Swiper
-              style={{
-                // @ts-ignore
-                "--swiper-pagination-color": theme.palette.primary.main,
-                paddingBottom: '2rem',
-              }}
-              observeParents={  true }
-              observer={  true }
-              pagination={{
-                dynamicBullets: true,
-              }}
-              modules={[ Pagination ]}
-              slidesPerView="auto"
-              spaceBetween={ 10 }
-              centeredSlides={ false }
-              onSlideChange={ () => {
-                setSwiperState( {
-                  isEnd: swiperDef.isEnd,
-                  isBeginning: swiperDef.isBeginning
-                } );
-              } }
-              onSwiper={ (swiper) => {
-                setSwiperDef( swiper );
-                setSwiperState( {
-                  isEnd: false,
-                  isBeginning: true
-                });
-              }}
-              breakpoints={{
-                "640": {
-                  "slidesPerView": 2,
-                  "spaceBetween": 10
-                },
-                "768": {
-                  "slidesPerView": 2,
-                  "spaceBetween": 40
-                },
-                "1024": {
-                  "slidesPerView": 4,
-                  "spaceBetween": 10
-                }
-              }}
-              freeMode={true}
-          >
-            { ( data && data.length > 0 ) && data.map( (item, index ) => (
-                <SwiperSlide>
-                  <Box key={index} sx={boxStyles}>
-                    <PropertyCard key={ index } data={ item.node }/>
-                  </Box>
-                </SwiperSlide>
+                    <Grid item>
+                        <Typography sx={{fontWeight: 600, mb: 1}} variant="h5">{subtitle}</Typography>
+                    </Grid>
+                    <Grid sx={{
+                        display: {
+                            xs: 'none',
+                            md: 'flex'
+                        },
+                    }} item
+                    >
+                        <Button
+                            sx={{ textTransform: 'none' }}
+                            color="primary"
+                            onClick={ () => navigate( '/propiedades' ) }
+                            variant="contained"
+                        >
+                            Ver más propiedades
+                        </Button>
+                        <Box>
+                            <IconButton
+                                sx={ arrowButtonStyles }
+                                disabled={ swiperState.isBeginning }
+                                onClick={ () => swiperDef ? swiperDef.slidePrev() : '' }
+                            >
+                                <ChevronLeft/>
+                            </IconButton>
+                            <IconButton
+                                sx={{
+                                    borderRadius: 2,
+                                    height: 40
+                                }}
+                                disabled={ swiperState.isEnd }
+                                onClick={ () => swiperDef ? swiperDef.slideNext() : '' }
+                            >
+                                <ChevronRight/>
+                            </IconButton>
+                        </Box>
+                    </Grid>
+                </Grid>
+                <Swiper
+                    style={{
+                        // @ts-ignore
+                        "--swiper-pagination-color": theme.palette.primary.main,
+                        paddingBottom: '2rem',
+                    }}
+                    observeParents={  true }
+                    observer={  true }
+                    pagination={{
+                        dynamicBullets: true,
+                    }}
+                    modules={[ Pagination ]}
+                    slidesPerView="auto"
+                    spaceBetween={ 10 }
+                    centeredSlides={ false }
+                    onSlideChange={ () => {
+                        setSwiperState( {
+                            isEnd: swiperDef.isEnd,
+                            isBeginning: swiperDef.isBeginning
+                        } );
+                    } }
+                    onSwiper={ (swiper) => {
+                        setSwiperDef( swiper );
+                        setSwiperState( {
+                            isEnd: false,
+                            isBeginning: true
+                        });
+                    }}
+                    breakpoints={{
+                        "640": {
+                            "slidesPerView": 2,
+                            "spaceBetween": 10
+                        },
+                        "768": {
+                            "slidesPerView": 2,
+                            "spaceBetween": 40
+                        },
+                        "1024": {
+                            "slidesPerView": 3,
+                            "spaceBetween": 10
+                        },
+                        "1280": {
+                            "slidesPerView": 4,
+                            "spaceBetween": 10
+                        }
+                    }}
+                    freeMode={true}
+                >
+                    { ( data && data.length > 0 ) && data.map( (item, index ) => (
+                        <SwiperSlide>
+                            <Box key={index} sx={boxStyles}>
+                                <PropertyCard key={ index } data={ item.node }/>
+                            </Box>
+                        </SwiperSlide>
 
-            )) }
-          </Swiper>
-          <Box
-              sx={{
-                mt: 4,
-                display: {
-                  xs: 'flex',
-                  md: 'none'
-                },
-                justifyContent: 'center'
-              }}
-          >
-            <Button
-                sx={{ textTransform: 'none' }}
-                color="primary"
-                onClick={ () => navigate( '/propiedades' ) }
-                variant="contained"
-            >
-              Ver más propiedades
-            </Button>
-          </Box>
-        </Container>
-      </>
-  )
+                    )) }
+                </Swiper>
+                <Box
+                    sx={{
+                        mt: 4,
+                        display: {
+                            xs: 'flex',
+                            md: 'none'
+                        },
+                        justifyContent: 'center'
+                    }}
+                >
+                    <Button
+                        sx={{ textTransform: 'none' }}
+                        color="primary"
+                        onClick={ () => navigate( '/propiedades' ) }
+                        variant="contained"
+                    >
+                        Ver más propiedades
+                    </Button>
+                </Box>
+            </Container>
+        </>
+    )
 }
 
 export default React.forwardRef(PropertySlider);
