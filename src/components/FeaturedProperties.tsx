c0437c1de2d603f483f1a768e251ddb1c3096d6a
iemm96 <iemm96@gmail.com>
2022-07-26 01:19:05 -0500

    Deleted unnecesary code

--- src/components/FeaturedProperties.tsx
+++ src/components/FeaturedProperties.tsx
@@ -1,14 +1,13 @@
 import * as React from "react";
 import SliderComponent from "./SliderComponent"
 import { useState } from "react"
-import { fetchRecords } from "../actions/fetchRecords"
 import { graphql, useStaticQuery } from "gatsby";
 
 const title:string = "¡Tu mejor opción!";
 const subtitle:string = "Propiedades destacadas";
 
 const FeaturedProperties = () => {
-  const [ properties, setProperties ] = useState<any>([]);
+    const [ properties, setProperties ] = useState<any>([]);
 
     const data = useStaticQuery(graphql`
         query FeaturedItems {
@@ -21,12 +20,9 @@ const FeaturedProperties = () => {
                             }
                         }
                         name
-                        
                         uid
                         measures_unit
                         isFeatured
-                        
-                        
                     }
                 }
             }
@@ -51,73 +47,30 @@ const FeaturedProperties = () => {
                 }
             }
         }
-
-
-
     `);
 
-  /*
-    const { allProperties } = useStaticQuery(graphql`
-      query FeaturedPropertiesQuery {
-          allProperties(filter: {isFeatured: {eq: true}}) {
-              nodes {
-                  coverImage {
-                      childImageSharp {
-                          gatsbyImageData(width: 280, placeholder: BLURRED, formats: [AUTO, WEBP, AVIF])
-                      }
-                  }
-                  name
-                  price
-                  uid
-                  measures_unit
-                  features {
-                      name
-                  }
-                  currency
-                  location {
-                      name
-                  }
-                  isFeatured
-              }
-          }
-      }
-  `);
-  */
 
     React.useEffect(() => {
         if( data ) {
             if( data.allProject.edges ) {
-
                 setProperties( data.allProject.edges.concat( data.allProperty.edges ) )
             }
         }
     },[ data ])
 
-  const getFeaturedProperties = async () => {
-      const { properties } = await fetchRecords('properties' );
-
-      const arrFeaturedProperties = [];
-
-      properties.map( item => (
-        item.isFeatured && arrFeaturedProperties.push( item )
-      ))
-
-      setProperties( arrFeaturedProperties );
-  }
-
-  return(
-    <>
-      {
-          properties.length > 0 && (
-            <SliderComponent
-              title={title}
-              subtitle={subtitle}
-              data={ properties }
-            />
-          )
-      }
-    </>
-  )
+    return(
+        <>
+            {
+                properties.length > 0 && (
+                    <SliderComponent
+                        title={title}
+                        subtitle={subtitle}
+                        data={ properties }
+                    />
+                )
+            }
+        </>
+    )
 }
 
 
