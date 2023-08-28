a1f312bf0acf211933e7ec6355684121cc461f78
iemm96 <iemm96@gmail.com>
2023-08-27 22:00:31 -0600

    Update FeaturedProperties.tsx

--- src/components/FeaturedProperties.tsx
+++ src/components/FeaturedProperties.tsx
@@ -2,15 +2,17 @@ import * as React from "react";
 import SliderContainer from "./SliderContainer"
 import { useState } from "react"
 import { graphql, useStaticQuery } from "gatsby";
+import PropertyCard from "./PropertyCard";
+import Grid from "@mui/material/Grid";
 const title:string = "¡Tu mejor opción!";
 const subtitle:string = "Propiedades destacadas";
 
-const FeaturedProperties = ({ attached, fullScreen }:{ attached?:boolean, fullScreen?:boolean }) => {
+const FeaturedProperties = ({ attached, fullScreen, enableSlider = false}:{ attached?:boolean, fullScreen?:boolean, enableSlider?: boolean }) => {
     const [ properties, setProperties ] = useState<any>([]);
 
     const data = useStaticQuery(graphql`
         query FeaturedItems {
-            allProperty(filter: {isFeatured: {eq: true}, isVisible: {eq: true}}) {
+            allProperty(filter: {isFeatured: {eq: true}, isVisible: {eq: true}}, limit: 4) {
                 edges {
                     node {
                         coverImage {
@@ -55,16 +57,31 @@ const FeaturedProperties = ({ attached, fullScreen }:{ attached?:boolean, fullSc
             {
                 properties.length > 0 && (
                     <div style={
-                        fullScreen ? fullScreenStyles : {}
+                        fullScreen && enableSlider ? fullScreenStyles : {}
                     }>
-                        <SliderContainer
-                            title={ title }
-                            fullScreen={ fullScreen }
-                            attached={ attached }
-                            subtitle={ subtitle }
-                            data={ properties }
-                            viewMoreButtonRedirectPath="propiedades"
-                        />
+                        {
+                            enableSlider ? (
+                                <SliderContainer
+                                    title={ title }
+                                    fullScreen={ fullScreen }
+                                    attached={ attached }
+                                    subtitle={ subtitle }
+                                    data={ properties }
+                                    viewMoreButtonRedirectPath="propiedades"
+                                />
+                            ) : (
+                                <Grid alignContent="center" spacing={ 2 } direction="row" container>
+                                    {
+                                        properties.map((property, index) => (
+                                            <Grid xs={ 12 } md={ 3 }  item>
+                                                <PropertyCard key={index} data={property.node}/>
+                                            </Grid>
+                                        ))
+                                    }
+                                </Grid>
+                            )
+                        }
+
                     </div>
                 )
             }
