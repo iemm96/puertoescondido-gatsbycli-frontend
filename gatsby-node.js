acbde6f2c128db845f35c00849e5ca10d0b3d67c
Emmanuel Fernández <iemm96@gmail.com>
2024-06-22 00:29:13 -0600

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -30,6 +30,9 @@ exports.createPages = async ({ graphql, actions }) => {
             features {
               name
             }
+            category {
+              name
+            }
           }
         }
       }
