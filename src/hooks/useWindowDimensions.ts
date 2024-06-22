ba41c198c750d62cdb4c21cefad4f9bb064f5ac5
Emmanuel Fernández <iemm96@gmail.com>
2024-06-22 17:24:55 -0600

    Update useWindowDimensions.ts

--- src/hooks/useWindowDimensions.ts
+++ src/hooks/useWindowDimensions.ts
@@ -1,25 +1,25 @@
-import { useState, useEffect } from 'react';
-import { window } from "browser-monads";
+import { useState, useEffect } from "react"
+import { window } from "browser-monads"
 
 function useWindowDimensions() {
   const [windowSize, setWindowSize] = useState({
     width: undefined,
     height: undefined,
-  });
+  })
 
   useEffect(() => {
     function handleResize() {
       setWindowSize({
         width: window.innerWidth,
         height: window.innerHeight,
-      });
+      })
     }
-    window.addEventListener("resize", handleResize);
-    handleResize();
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
+    window.addEventListener("resize", handleResize)
+    handleResize()
+    return () => window.removeEventListener("resize", handleResize)
+  }, [])
 
-  return windowSize;
+  return windowSize
 }
 
-export default useWindowDimensions;
+export default useWindowDimensions
