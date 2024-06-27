be7c64939ede4cd9db5bcde8c988e82617babf43
Emmanuel Fernández <iemm96@gmail.com>
2024-06-27 08:58:33 -0600

    Update Carousel.tsx

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,12 +1,12 @@
-import * as React from "react";
-import styled from "styled-components";
-import {GatsbyImage, getImage} from "gatsby-plugin-image";
-import {graphql, useStaticQuery} from "gatsby";
+import * as React from "react"
+import styled from "styled-components"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import { graphql, useStaticQuery } from "gatsby"
 
 type CarouselTypes = {
-  styles?: React.CSSProperties;
-  images?: any,
-  data?:any
+  styles?: React.CSSProperties
+  images?: any
+  data?: any
 }
 
 const StyledSlideDiv = styled.div`
@@ -14,81 +14,67 @@ const StyledSlideDiv = styled.div`
   height: 100%;
   position: absolute;
   transition: opacity ease-in-out 0.4s;
-  opacity: ${props => props.animation ? 1 : 0};
-`;
-
-const dataSlider = [
-  {
-    image: "banner-7.jpg",
-  },
-  {
-    image: "banner-3.jpg",
-  },
-  {
-    image: "banner-5.JPG",
-  }
-]
-const Carousel = ({ styles }:CarouselTypes) => {
-  const [ images, setImages ] = React.useState<[]>();
+  opacity: ${props => (props.animation ? 1 : 0)};
+`
+const Carousel = ({ styles }: CarouselTypes) => {
+  const [images, setImages] = React.useState<[]>()
   const { allBanner } = useStaticQuery(graphql`
     query BannerQuery {
-      allBanner(filter: {isActive: {eq: true}}) {
+      allBanner(filter: { isActive: { eq: true } }) {
         nodes {
           image {
             childImageSharp {
               gatsbyImageData(
-                breakpoints: [750, 1080, 1366, 1920],
-                quality: 50,
-                formats: [ WEBP, AVIF]
+                breakpoints: [750, 1080, 1366, 1920]
+                quality: 50
+                formats: [WEBP, AVIF]
               )
             }
           }
         }
       }
     }
-  `);
+  `)
 
   React.useEffect(() => {
     setImages(allBanner?.nodes)
-  },[allBanner])
+  }, [allBanner])
 
-  const [slideIndex, setSlideIndex] = React.useState(1);
+  const [slideIndex, setSlideIndex] = React.useState(1)
 
   React.useEffect(() => {
-    setTimeout(nextSlide,5000);
-  },[slideIndex]);
+    setTimeout(nextSlide, 5000)
+  }, [slideIndex])
 
   const nextSlide = () => {
-    if(slideIndex !== dataSlider.length){
+    if (slideIndex !== allBanner?.nodes.length) {
       setSlideIndex(slideIndex + 1)
-    }
-    else if (slideIndex === dataSlider.length){
+    } else if (slideIndex === allBanner.nodes.length) {
       setSlideIndex(1)
     }
   }
 
-  return(
+  return (
     <>
       <div style={styles ? styles : {}}>
-        {
-          images && images.map((image:any,index) => (
+        {images &&
+          images.map((image: any, index) => (
             <StyledSlideDiv animation={slideIndex === index + 1}>
               {/*@ts-ignore */}
               <GatsbyImage
-                  image={ getImage( image?.image ) }
-                  style={{
-                    width: '100%',
-                    height: 800,
-                    objectPosition: 'cover',
-                  }}
-                  alt={ 'data.title' }
+                image={getImage(image?.image)}
+                style={{
+                  width: "100%",
+                  height: 800,
+                  objectPosition: "cover",
+                }}
+                alt={"data.title"}
               />
             </StyledSlideDiv>
-          ))
-        }
+          ))}
       </div>
     </>
   )
 }
 
-export default Carousel;
+export default Carousel
